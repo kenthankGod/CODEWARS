19c1e99989d3736fca01c5fc568f19eb9c02497c
Ken ThankGod <kenthankgod0803@gmail.com>
2022-05-25 13:48:29 -0700

    added new codewars challenge today

---
+++ ConvertStringToArray.js
@@ -0,0 +1,7 @@
+//Convert a string to an array of strings
+// convert strings into seperate strings in an array
+function stringToArray(string) {
+  let str = string.split(" ");
+
+  return str;
+} console.log(stringToArray("I love arrays they are my favorite"));
