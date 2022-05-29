8152ba5a20d812e44e89e21416e861ad1313c54b
Ken ThankGod <kenthankgod0803@gmail.com>
2022-05-29 12:52:50 -0700

    added new codewars challenge

---
+++ divideByFirstNumber.js
@@ -0,0 +1,10 @@
+//  divide first number
+function isDivideBy(number, a, b) {
+  number % a === 0 && number % b === 0 ? true : false
+                    // OR
+// if (number % a === 0 && number % b === 0) {
+//     return true
+// } else {
+//     return false
+// }
+}
