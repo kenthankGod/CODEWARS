4b154ec127a071e8412a59f94a21d12e8ae124cf
Ken ThankGod <kenthankgod0803@gmail.com>
2022-05-31 09:40:11 -0700

    added new codewars challenge

---
+++ booleanToString.js
@@ -0,0 +1,12 @@
+ 
+//Convert a Boolean to a String
+
+// Implement a function which convert the given boolean value
+// into its string representation.
+
+// Note: Only valid inputs will be given.
+
+// just use "toString()" method on your value
+function booleanToString(b){
+   return b.toString()
+}console.log(booleanToString(4))
