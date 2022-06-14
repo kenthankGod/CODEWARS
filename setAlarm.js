24c095d48521d62101dffbbcd6c521de6cfe69fd
Ken ThankGod <kenthankgod0803@gmail.com>
2022-06-14 15:02:16 -0700

    added new codewars

---
+++ setAlarm.js
@@ -0,0 +1,34 @@
+/* 
+Set alarm
+
+Write a function named setAlarm which receives two parameters. The first parameter, employed, is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
+
+The function should return true if you are employed 
+and not on vacation (because these are the circumstances under which you need to set an alarm). 
+It should return false otherwise. Examples:
+
+setAlarm(true, true) -> false
+setAlarm(false, true) -> false
+setAlarm(false, false) -> false
+setAlarm(true, false) -> true
+
+*/
+
+function setAlarm(employed, vacation) {
+
+    return (employed && !vacation ) ? true : false
+// if you wanna wild out you could do this lol
+
+/* 👀 */
+
+// if (employed === true && vacation === true) {
+//     return false
+// } else if(employed === false && vacation === true){
+//     return false
+// } else if(employed === false && vacation === false){
+//     return false
+// } else if(employed === true && vacation === false){
+//     return true
+// }
+
+} console.log(setAlarm(true, false));
